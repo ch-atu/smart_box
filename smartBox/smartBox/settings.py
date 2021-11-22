53952c33b5ce27efc7d18ccfdb96234423b48818
chatu <634930192@qq.com>
2021-11-22 15:52:55 +0800

    update

--- smartBox/smartBox/settings.py
+++ smartBox/smartBox/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'smartBox.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
+        'HOST': '119.29.139.149',
         'PORT': '3306',
         'NAME': 'smartBox',
         'USER': 'root',
