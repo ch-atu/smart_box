681122afe441e16f84cd3b1aa74b16621cdf436a
chatu <634930192@qq.com>
2021-11-26 11:33:17 +0800

    update

---
+++ socket_test4.py
@@ -0,0 +1,51 @@
+
+import socket
+import re
+import threading
+
+
+class WSGIServer(object):
+
+    def __init__(self, server_address):
+        # 创建一个tcp套接字
+        self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 允许立即使用上次绑定的port
+        self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # 绑定
+        self.listen_socket.bind(server_address)
+        # 变为被动，并制定队列的长度
+        self.listen_socket.listen(128)
+
+    def serve_forever(self):
+        "循环运行web服务器，等待客户端的链接并为客户端服务"
+        while True:
+            # 等待新客户端到来
+            client_socket, client_address = self.listen_socket.accept()
+            print(client_address)
+            new_process = threading.Thread(target=self.handleRequest, args=(client_socket,))
+            new_process.start()
+
+            # 因为线程是共享同一个套接字，所以主线程不能关闭，否则子线程就不能再使用这个套接字了
+            # client_socket.close()
+
+    def handleRequest(self, client_socket):
+        "用一个新的进程，为一个客户端进行服务"
+        recv_data = client_socket.recv(1024).decode('gbk')
+        print(recv_data)
+        client_socket.send('你好'.encode('gbk'))
+        client_socket.close()
+
+
+# 设定服务器的端口
+SERVER_ADDR = (HOST, PORT) = "", 8888
+
+
+def main():
+    httpd = WSGIServer(SERVER_ADDR)
+    print("web Server: Serving HTTP on port %d ...\n" % PORT)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
+
