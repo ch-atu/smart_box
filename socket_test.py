22d26c860d18852357b1649073033a885055143b
chatu <634930192@qq.com>
2021-11-29 14:32:17 +0800

    修改云盒API接口

--- socket_test.py
+++ socket_test.py
@@ -11,20 +11,20 @@ def main():
     # 3. 将手机设置为正常的 响铃模式(让默认的套接字由主动变为被动 listen)
     tcp_server_socket.listen(128)
 
-    print("-----1----")
+    print("-----开始监听！----")
     # 4. 等待别人的电话到来(等待客户端的链接 accept)
     new_client_socket, client_addr = tcp_server_socket.accept()
-    print("-----2----")
+    print("-----已经接受到请求！----")
 
-    print(client_addr)
+    print('客户端的信息是：', client_addr)
 
     # 接收客户端发送过来的请求
     recv_data = new_client_socket.recv(1024).decode('gbk')
-    print(recv_data, type(recv_data))
+    print('收到的数据是：', recv_data, type(recv_data))
 
     # 回送一部分数据给客户端
     # new_client_socket.send("hahahahai-----ok-----".encode("utf-8"))
-    new_client_socket.sendall("-----ok-----".encode("utf-8"))
+    new_client_socket.sendall("-----ok-----".encode("gbk"))
 
     # 关闭套接字
     new_client_socket.close()
@@ -35,16 +35,5 @@ if __name__ == "__main__":
     while True:
         main()
 
-    # a = bytes.hex('313233')
-    # print(a)
-    # print(hex(123))
-    # a = b'\x00\x00\x00\x00\x89\x9a\x9a\x9a'
-    # b = str(a)
-    # print(type(a), type(b))
-    # print(a, b)
-    # # print(a.decode('utf-8'))
-    # print(a.decode())
-    # b = '123456'
-    # print(b.encode())
 
 
